5b3fdb63ea6f2cdf98fc6c90b4305886c52e062a
8ehrad <zbehradz7@gmail.com>
2025-02-25 14:27:36 +0000

    Deploying to gh-pages from @ 8ehrad/8ehrad.github.io@ec88b0559f6df7b74e524eb6ac8734aef8a2ccc9 🚀

--- _app/immutable/chunks/experience.40322cd2.js
+++ _app/immutable/chunks/experience.40322cd2.js
@@ -1,4 +1,4 @@
-import{A as e}from"./UIcon.09b84335.js";import{g as t}from"./skills.ec885bde.js";import{C as i}from"./types.1ebf2f37.js";const o=[{slug:"technical-associate",company:"Informed Solutions",description:`
+import{A as e}from"./UIcon.68b1c94c.js";import{g as t}from"./skills.584c082b.js";import{C as i}from"./types.1ebf2f37.js";const o=[{slug:"technical-associate",company:"Informed Solutions",description:`
 - Automated farmers’ funding validation pipeline leveraging NLP and computer vision to reduce manual review time by 2 weeks per case.
 - Built a Power BI dashboard for delivery managers and recruitment team to make real-time data-driven decisions.
 - Optimised database design and data modelling, integrating it into interactive dashboards to provide stakeholders with actionable insights for efficient project and resource allocation.
